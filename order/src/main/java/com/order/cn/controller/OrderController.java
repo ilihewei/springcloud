2d4ab4134e3e977fb821a806290b692abaa75c10
ilihewei <1968895380@qq.com>
2020-03-29 17:33:05 +0800

    第一步的访问，restTemplate的配置

---
+++ order/src/main/java/com/order/cn/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.order.cn.controller;
+
+import com.order.cn.entity.CommentResult;
+import com.order.cn.entity.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    private static  String url="http://localhost:8001";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "order/get/{id}")
+    public CommentResult<Payment>  get(@PathVariable("id") Integer id){
+       return restTemplate.getForObject(url+"/payment/get/"+id,CommentResult.class);
+    }
+    @PostMapping(value = "order/create")
+    public CommentResult<Payment> create(Payment payment){
+        return  restTemplate.patchForObject(url+"/payment/create",payment,CommentResult.class);
+    }
+}
